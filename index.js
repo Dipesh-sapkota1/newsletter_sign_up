45e29cf408c3ef0e052fed8c6ddbd99ba70f7d2b
Dipesh-sapkota1 <sapkotadipes.com>
2024-06-21 19:49:57 +0545

    fixed success page showing email

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const emailInput = document.getElementById('email');
 const emailError = document.getElementById('emailError');
 form.noValidate = true;
 
+ 
+ 
+
+ 
+
 function handleSubmit(e) {
   e.preventDefault();
 
   const emailValue = emailInput.value;
-
   if (!emailValue) {
     emailError.innerHTML = '<strong>Please enter an email adderss</strong>';
   } else if (emailInput.validity.typeMismatch) {
     emailError.innerHTML = '<strong>Valid email required</strong>';
-  } else {
+  } else{
+    localStorage.setItem('userEmail', emailValue);
+    window.location.href = 'success.html';
     this.submit();
   }
+  
 }
 
 form.addEventListener('submit', handleSubmit);
@@ -22,7 +29,6 @@ emailInput.addEventListener('input', validateEmail);
 
 function validateEmail(e) {
   const isValid = e.target.validity.typeMismatch;
-
   if (isValid) {
     emailError.innerHTML = '<strong>Valid email required</strong>';
   } else {
